0260159dd4df633a027aca7cf13e0b78200fb4bd
Benjamin Arandjelovic-Vaughan <bfavaughan@gmail.com>
2025-08-17 10:11:24 -0400

    Completed front end pages for Clinic Management System

--- app/src/main/resources/static/js/components/doctorCard.js
+++ app/src/main/resources/static/js/components/doctorCard.js
@@ -1,3 +1,69 @@
+export function createDoctorCard(doctor){
+    const card = document.createElement("div");
+    card.classList.add("doctor-card");
+    
+    const role = localStorage.getItem("userRole");
+    
+    const infoDiv = document.createElement("div");
+    infoDiv.classList.add("doctor-info");
+
+    const name = document.createElement("h3");
+    name.textContent = doctor.name;
+    const specialization = document.createElement("h3");
+    specialization.textContent = doctor.specialization;
+    const email = document.createElement("h3");
+    email.textContent = doctor.email;
+    const availability = document.createElement("h3");
+    availability.textContent = doctor.availability.join(", ");
+
+    infoDiv.appendChild(name);
+    infoDiv.appendChild(specialization);
+    infoDiv.appendChild(email);
+    infoDiv.appendChild(availability);
+
+    const actionsDiv = document.createElement("div");
+    actionsDiv.classList.add("card-actions");
+
+    if (role === "admin") {
+        const removeBtn = document.createElement("button");
+        removeBtn.textContent = "Delete";
+        actionsDiv.appendChild(removeBtn);
+
+        removeBtn.addEventListener("click", async () => {
+            // 1. Confirm deletion
+            if (confirm('Are you sure you would like to delete '+doctor.name+' ?') == true) {
+                x = "You pressed OK!"; 
+                // 2. Get token from localStorage
+                const token = localStorage.getItem("token");
+                // 3. Call API to delete
+                // 4. On success: remove the card from the DOM
+            }
+           
+            
+        });
+        actionsDiv.appendChild(removeBtn);
+    } else if (role === "patient") {
+        const bookNow = document.createElement("button");
+        bookNow.textContent = "Book Now";
+        bookNow.addEventListener("click", () => {
+          alert("Patient needs to login first.");
+        });
+    } else if (role === "loggedPatient") {
+        const bookNow = document.createElement("button");
+        bookNow.textContent = "Book Now";
+        bookNow.addEventListener("click", async (e) => {
+          const token = localStorage.getItem("token");
+          const patientData = await getPatientData(token);
+          showBookingOverlay(e, doctor, patientData);
+        });
+    }
+
+    card.appendChild(infoDiv);
+    card.appendChild(actionsDiv);
+
+}
+
+
 /*
 Import the overlay function for booking appointments from loggedPatient.js
 
